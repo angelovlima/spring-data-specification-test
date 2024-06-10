76ffb44d5fa62489f8754b910f714ddcc40dc589
angelovlima <angelovictor2001@hotmail.com>
2024-06-10 19:05:15 -0300

    first commit

---
+++ src/main/java/com/example/api/spring_data_specification_test/DataSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.spring_data_specification_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSpecificationApplication.class, args);
+	}
+
+}
